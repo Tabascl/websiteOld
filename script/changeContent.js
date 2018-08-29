54e15b490613135c6a5565dc89e77094ddf8d0e8
Tabascl <simonwilde93@gmail.com>
2018-08-29 02:15:56 +0200

    Make page changes for mobile, remove zip

--- script/changeContent.js
+++ script/changeContent.js
@@ -1,8 +1,13 @@
+function init() {
+    $("#homebutton").on('click touchstart', function () { toggleSite("home"); });
+    $("#projectsbutton").on('click touchstart', function () { toggleSite("projects"); });
+}
+
 function toggleSite(target) {
     var toShow = document.getElementById(target);
-    var divs = document.getElementsByClassName("dyntext")
+    var divs = document.getElementsByClassName("dyntext");
 
-    for (var i = 0; i<divs.length;i++) {
+    for (var i = 0; i < divs.length; i++) {
         divs[i].style.opacity = 0;
         divs[i].style.display = "none";
     }
@@ -10,3 +15,5 @@ function toggleSite(target) {
     toShow.style.opacity = 1;
     toShow.style.display = 'block';
 }
+
+window.onload = function () { init(); };
